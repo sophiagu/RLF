07817aabb88069f165d1450e8d885145ed89baf7
Sophia Gu <14866379+sophiagu@users.noreply.github.com>
2020-08-10 08:29:45 -0400

    Add files via upload

--- gym-rlf/gym_rlf/envs/mean_reversion_env.py
+++ gym-rlf/gym_rlf/envs/mean_reversion_env.py
@@ -7,109 +7,103 @@ import tempfile
 
 from gym import spaces
 from gym_rlf.envs.rlf_env import RLFEnv
-from Utils.ou_process import get_ou_sequence
+from gym_rlf.envs.Parameters import TickSize, LotSize, M, K, Lambda, sigma, kappa, p_e, L
+
+# OpenAI Baselines recommends to normalize continuous action space because the Baselines
+# agents only sample actions from a standard Gaussian.
+# We use a space normalizer to rescale action space to [-LotSize * K, LotSize * K].
+action_space_normalizer = LotSize * K
+
+MAX_HOLDING = LotSize * M
+MIN_PRICE = round(TickSize, 2)
+MAX_PRICE = round(TickSize * 1000, 2)
 
 
 class MeanReversionEnv(RLFEnv):
-  ACTION = {
-      0 : -100,
-      1 : +100,
-  }
-
   def __init__(self):
     super(MeanReversionEnv, self).__init__()
-    self.temp_name = next(tempfile._get_candidate_names())
-    
-    self.equilibrium_price = 100.
-    self.theta, self.sigma, self.kappa, self.length, self.low, self.high = .1, .2, 1.0e-4, 1000, -2., .301
-    self.linear_cost_multiplier, self.quadratic_cost_multiplier = .2, .15
-    self.render_count = 0
-    
-    # self.observation_space = spaces.Dict({
-    #   'position': spaces.Box(low=-100*self.length, high=100*self.length, shape=(1,)),
-    #   'price': spaces.Box(low=0., high=self.equilibrium_price * 10.**self.high, shape=(1,)),
-    # })
-    # Use a Box to represent an observation splace with the first param being position and the second being price.
+    # Save plots to a newly created folder under mean_reversion_plots/.
+    self.folder_name = 'mean_reversion_plots/' + next(tempfile._get_candidate_names())
+
+    self.action_space = spaces.Box(low=-1, high=1, shape=(1,), dtype=np.float64)
+    # Use a Box to represent an observation space with the first param being (position)
+    # and the second param being (price).
     self.observation_space = spaces.Box(
-      low=np.array([-100*self.length, 0,]),
-      high=np.array([100*self.length, self.equilibrium_price * 10.**self.high]),
+      low=np.array([-MAX_HOLDING, MIN_PRICE]),
+      high=np.array([MAX_HOLDING, MAX_PRICE]),
       shape=(2,),
-      dtype=np.float32)
-    
-    # simple discrete action space
-    # self.action_space = spaces.Discrete(2) # buy or sell 100 shares of the underlying
-    
-    # continuous action space
-    self.action_space = spaces.Box(low=-1, high=1, shape=(1,), dtype=np.float32)
-    # OpenAI baselines/stable-baselines recommends to normalize continuous action space
-    self.action_space_normalizer = 100
-  
+      dtype=np.float64)
+
+  def next_price(self, p):
+      x = np.log(p / p_e)
+      rn = np.random.normal(0, 1.0, 1)[0]
+      x = x - Lambda * x + sigma * rn
+      p_new = p_e * np.exp(x)
+      p_new = min(p_new, MAX_PRICE)
+      p_new = max(p_new, MIN_PRICE)
+      return p_new
+
   def reset(self):
-    self.count = 0
-    self.price_process = get_ou_sequence(self.theta, self.sigma, self.length+1, self.low, self.high)
-    self.price = self.equilibrium_price
-    self.position = np.zeros(self.length+1)
-    self.position[0] = 1000
-    self.cumulative_rewards = [0]
-    
+    self.render_counts = 0
+    self.step_counts = 0
+    self.prices = np.zeros(L+1)
+    self.prices[0] = p_e
+    self.positions = np.zeros(L+1)
+    self.rewards = np.zeros(L+1)
+    self.profits = np.zeros(L+1)
+    self.costs = np.zeros(L+1)
     return self.getState()
 
   def getState(self):
-    # return {'position': self.position[self.count], 'price': self.price}
-    return np.array([self.position[self.count], self.price]).astype(np.float32)
-
-  def step(self, action):
-    # make sure we only trade integer number of shares
-    a = round(action[0] * self.action_space_normalizer)
-    # bound the action
-    a = min(100*self.length-self.position[self.count], a) if\
-     self.position[self.count] > 0 else min(100*self.length, a)
-    # a = max(-100*self.length-self.position[self.count], a)
-    # if long only: make sure we don't own negative number of shares
-    a = max(-self.position[self.count], a)
-    new_price = self.equilibrium_price * 10**self.price_process[self.count]
-    profit = (new_price - self.price) * self.position[self.count]
-    cost = a * new_price
-    impact = self.linear_cost_multiplier * np.abs(a) + self.quadratic_cost_multiplier * a**2
-    PnL = profit - cost - impact
-    reward = PnL - self.kappa * PnL**2
+    return np.array([self.positions[self.step_counts], self.prices[self.step_counts]]).astype(np.float64)
     
-    self.count += 1
-    done = self.count % self.length == 0
+  def step(self, action):
+    a = round(action[0] * action_space_normalizer)
 
-    self.price = new_price
-    # self.position[self.count] = self.position[self.count-1] + MeanReversionEnv.ACTION[action[0]]
-    self.position[self.count] = self.position[self.count-1] + a
-    if done:
-      PnL = self.price * self.position[self.count] - self.linear_cost_multiplier * np.abs(self.position[self.count])\
-       + self.quadratic_cost_multiplier * self.position[self.count]**2
-      reward += PnL - self.kappa * PnL**2
-    self.cumulative_rewards.append(self.cumulative_rewards[-1] + reward)
+    old_pos = self.positions[self.step_counts]
+    old_price = self.prices[self.step_counts]
+    self.step_counts += 1
+    new_pos = self.positions[self.step_counts] = max(min(old_pos + a, MAX_HOLDING), -MAX_HOLDING)
+    new_price = self.prices[self.step_counts] = self.next_price(old_price)
 
-    return self.getState(), reward, done, {}
+    trade_size = abs(new_pos - old_pos)
+    cost = TickSize * (trade_size + 1e-2 * trade_size**2)
+    PnL = (new_price - old_price) * old_pos - cost
+    self.costs[self.step_counts] = cost
+    self.profits[self.step_counts] = PnL + cost
+    self.rewards[self.step_counts] = PnL - .5 * kappa * PnL**2
+
+    done = self.step_counts == L
+    if done: # liquidate the remaining new_pos shares of the stock
+      add_cost = TickSize * (abs(new_pos) + 1e-2 * new_pos**2)
+      add_PnL = new_price * new_pos - add_cost
+      self.costs[self.step_counts] += add_cost
+      self.profits[self.step_counts] += add_PnL + add_cost
+      self.rewards[self.step_counts] += add_PnL - .5 * kappa * add_PnL**2
+
+    return self.getState(), self.rewards[self.step_counts], done, {}
  
   def render(self, mode='human'):
     if mode == 'human':
-      folder_name = 'mean_reversion_plots/{}'.format(self.temp_name)
-      if not os.path.exists(folder_name):
-        os.makedirs(folder_name)
-            
-      if (self.render_count+1) % 1 == 0: # only render every x step(s)
-        t = np.linspace(0, self.length, self.length+1)
-        fig, axs = plt.subplots(3, 1, figsize=(16, 24), constrained_layout=True)
+      self.render_counts += 1
+      if not os.path.exists(self.folder_name):
+        os.makedirs(self.folder_name)
 
-        axs[0].plot(t, self.equilibrium_price * 10**self.price_process, label='price')
-        axs[1].plot(t, self.position, label='position')
-        axs[2].plot(t, self.cumulative_rewards, label='cumulative P/L')
-        axs[0].set_ylabel('price')
-        axs[1].set_ylabel('position')
-        axs[2].set_ylabel('cumulative P/L')
-        axs[0].legend()
-        axs[1].legend()
-        axs[2].legend()
-        plt.title('Out-of-sample simulation of RL agent')
-        plt.xlabel('steps')
-        plt.savefig('mean_reversion_plots/{}/plot_{}.png'.format(self.temp_name, self.render_count))
-        plt.close()
-
-      self.render_count += 1
+      print('sharpe ratio =', 16 * np.mean(self.rewards) / np.std(self.rewards))
+      t = np.linspace(0, L, L+1)
+      fig, axs = plt.subplots(3, 1, figsize=(16, 24), constrained_layout=True)
+      axs[0].plot(t, self.prices)
+      axs[1].plot(t, self.positions)
+      axs[2].plot(t, np.cumsum(self.rewards))
+      axs[0].set_ylabel('price')
+      axs[1].set_ylabel('position')
+      axs[2].set_ylabel('cumulative P/L')
+      plt.title('Out-of-sample simulation of RL agent')
+      plt.xlabel('steps')
+      plt.savefig('{}/plot_{}.png'.format(self.folder_name, self.render_counts))
+      plt.close()
+      plt.plot(t, np.cumsum(self.costs), label='cumulative costs')
+      plt.plot(t, np.cumsum(self.profits), label='cumulative profits')
+      plt.legend()
+      plt.savefig('{}/costs_and_profits_plot_{}.png'.format(self.folder_name, self.render_counts))
+      plt.close()
