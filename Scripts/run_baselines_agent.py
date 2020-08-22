5a7c599c300d3a7b4046a28c8c6e0504125f7f67
Sophia Gu <14866379+sophiagu@users.noreply.github.com>
2020-08-21 21:10:36 -0400

    Add files via upload

--- Scripts/run_baselines_agent.py
+++ Scripts/run_baselines_agent.py
@@ -9,6 +9,7 @@ import argparse
 import multiprocessing
 import numpy as np
 import optuna
+import os
 
 from functools import partial
 from utils import make_env, ppo2_params, a2c_params
@@ -20,38 +21,57 @@ from stable_baselines import A2C, PPO2
 
 NUM_CPU = multiprocessing.cpu_count()
 
-def train(env_id, agent, model_params, is_evaluation, evaluation_steps, train_steps):
+def _train(env_id, agent, model_params, total_steps, is_evaluation=False):
   if is_evaluation: # evaluate_policy() must only take one environment
     envs = SubprocVecEnv([make_env(env_id)])
   else:
     envs = SubprocVecEnv([make_env(env_id) for _ in range(NUM_CPU)])
   envs = VecNormalize(envs) # normalize the envs during training and evaluation
 
-  if agent == 'ppo2':
-    model = PPO2(MlpLstmPolicy, envs, nminibatches=1, verbose=1, **model_params)
-  elif agent == 'a2c':
-    model = A2C(MlpLstmPolicy, envs, verbose=1, **model_params)
-
-  if is_evaluation:
-    model.learn(total_timesteps=evaluation_steps)
+  if not is_evaluation and os.path.exists(agent + '_' + env_id):
+    if agent == 'ppo2':
+      model = PPO2.load(agent + '_' + env_id)
+    elif agent == 'a2c':
+      model = A2C.load(agent + '_' + env_id)
   else:
-    model.learn(total_timesteps=train_steps)
+    if agent == 'ppo2':
+      model = PPO2(MlpLstmPolicy, envs, nminibatches=1, verbose=1, **model_params)
+    elif agent == 'a2c':
+      model = A2C(MlpLstmPolicy, envs, verbose=1, **model_params)
 
+  model.learn(total_timesteps=total_steps)
   return envs, model
   
-def search_hparams(env_id, agent, evaluation_steps, train_steps, trial):
+def _search_hparams(env_id, agent, total_steps, trial):
   if agent == 'ppo2':
     model_params = ppo2_params(trial)
   elif agent == 'a2c':
     model_params = a2c_params(trial)
 
-  envs, model = train(env_id, agent, model_params, True, evaluation_steps, train_steps)
+  envs, model = _train(env_id, agent, model_params, total_steps, True)
   mean_reward, _ = evaluate_policy(model, envs, n_eval_episodes=10)
   
   envs.close()
   # Negate the reward because Optuna maximizes the negative log likelihood.
   return -mean_reward
 
+def _eval_model(model, env_id, L, ob_shape, num_eps, plot=False):
+  test_env = SubprocVecEnv([make_env(env_id)])
+  sharpe_ratio = 0
+  for episode in range(num_eps):
+    # Padding zeros to the test env to match the shape of the training env.
+    zero_completed_obs = np.zeros((NUM_CPU,) + ob_shape)
+    zero_completed_obs[0, :] = test_env.reset()
+    state = None
+    for _ in range(L):
+      action, state = model.predict(zero_completed_obs, state=state, deterministic=True)
+      zero_completed_obs[0, :], reward, done, _ = test_env.env_method('step', action[0], indices=0)[0]
+    sharpe_ratio += test_env.env_method('get_sharpe_ratio', indices=0)[0]
+    if plot: test_env.env_method('render', indices=0)
+
+  test_env.close()
+  return sharpe_ratio / num_eps
+  
 
 if __name__ == '__main__':
   logger.set_level(logger.ERROR)
@@ -68,8 +88,8 @@ if __name__ == '__main__':
   parser.add_argument('--evaluation_steps', type=int, default=500000,
                       help=('Number of total timesteps that the model runs when evaluating hyperparameters.'
                             'This number must be a multiple of the environment episode size L.'))
-  parser.add_argument('--train_steps', type=int, default=1000000,
-                      help=('Number of total timesteps that the model runs during training.'
+  parser.add_argument('--max_train_steps', type=int, default=1000000,
+                      help=('Max number of total timesteps that the model runs during training.'
                             'This number must be a multiple of the environment episode size L.'))
   args = parser.parse_args()
   agent = args.agent.lower()
@@ -93,7 +113,7 @@ if __name__ == '__main__':
 
   if args.optimize:
     study.optimize(
-      partial(search_hparams, env_id, agent, args.evaluation_steps, args.train_steps),
+      partial(_search_hparams, env_id, agent, args.evaluation_steps),
       n_trials=args.num_trials,
       n_jobs=NUM_CPU)
     df = study.trials_dataframe(attrs=('number', 'value', 'params', 'state'))
@@ -102,25 +122,24 @@ if __name__ == '__main__':
     print('best value achieved =', study.best_value)
     print('best trial =', study.best_trial)
 
-  envs, model = train(env_id, agent, study.best_params, False, args.evaluation_steps, args.train_steps)
-  model.save(agent + '_' + args.env)
+  # Evaluate the model every 50 episodes. Stop when the evaluation result drops.
+  assert args.max_train_steps % (50 * L) == 0
+  best_sr = 0
+  for i in range(args.max_train_steps // (50 * L)):
+    envs, model = _train(env_id, agent, study.best_params, 50 * L)
+    sharpe_ratio = _eval_model(model, env_id, L, envs.observation_space.shape, 50)
+    print('sharpe ratio =', sharpe_ratio)
+    if sharpe_ratio > best_sr:
+      sharpe_ratio = best_sr
+      model.save(agent + '_' + args.env)
+    elif best_sr >= 2:
+      print('Training stopped after {} episodes with sharpe ratio {}.'.format(i + 1, best_sr))
+      break
 
   del model
   if agent == 'ppo2':
     model = PPO2.load(agent + '_' + args.env)
   elif agent == 'a2c':
     model = A2C.load(agent + '_' + args.env)
-
-  test_env = SubprocVecEnv([make_env(env_id)])
-  for episode in range(args.num_eps):
-    # Padding zeros to the test env to match the shape of the training env.
-    zero_completed_obs = np.zeros((NUM_CPU,) + envs.observation_space.shape)
-    zero_completed_obs[0, :] = test_env.reset()
-    state = None
-    for _ in range(L):
-      action, state = model.predict(zero_completed_obs, state=state, deterministic=True)
-      zero_completed_obs[0, :], reward, done, _ = test_env.env_method('step', action[0], indices=0)[0]
-    test_env.env_method('render', indices=0)
-
+  _eval_model(model, env_id, L, envs.observation_space.shape, args.num_eps, True)
   envs.close()
-  test_env.close()
