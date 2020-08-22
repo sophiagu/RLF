300d080f2f2fd35916fc9b1286eaef0d05f172c6
Sophia Gu <14866379+sophiagu@users.noreply.github.com>
2020-08-21 21:16:23 -0400

    Add files via upload

--- gym-rlf/gym_rlf/envs/mean_reversion_env.py
+++ gym-rlf/gym_rlf/envs/mean_reversion_env.py
@@ -7,6 +7,15 @@ from gym import spaces
 from gym_rlf.envs.rlf_env import RLFEnv, action_space_normalizer, MAX_HOLDING, MIN_PRICE, MAX_PRICE
 from gym_rlf.envs.Parameters import TickSize, Lambda, sigma, kappa, p_e
 
+PENALTY_WEIGHT = 1000
+MAX_PENALTY = 5000
+
+def func_property(s1, s2, a1, a2):
+  if (a1 - a2) * (s1 - s2) <= 0: # monotonic decreasing
+    return 0
+  else:
+    return min(MAX_PENALTY, PENALTY_WEIGHT * ((a1 - a2)/(s1 - s2))**2)
+    
 
 class MeanReversionEnv(RLFEnv):
   def __init__(self):
@@ -20,7 +29,7 @@ class MeanReversionEnv(RLFEnv):
       high=np.array([MAX_HOLDING, MAX_PRICE]),
       shape=(2,))
 
-  def next_price(self, p):
+  def _next_price(self, p):
     x = np.log(p / p_e)
     rn = np.random.normal(0, 1.0, 1)[0]
     x = x - Lambda * x + sigma * rn
@@ -32,54 +41,62 @@ class MeanReversionEnv(RLFEnv):
   def reset(self):
     super(MeanReversionEnv, self).reset()
 
-    return self.get_state()
+    return self._get_state()
 
-  def get_state(self):
-    return np.array([self.positions[self.step_counts], self.prices[self.step_counts]])
-    
+  def _get_state(self):
+    return np.array([self._positions[self._step_counts], self._prices[self._step_counts]])
+
   def step(self, action):
     ac = round(action[0] * action_space_normalizer)
 
-    old_pos = self.positions[self.step_counts]
-    old_price = self.prices[self.step_counts]
-    self.step_counts += 1
-    new_pos = self.positions[self.step_counts] = max(min(old_pos + ac, MAX_HOLDING), -MAX_HOLDING)
-    new_price = self.prices[self.step_counts] = self.next_price(old_price)
+    old_pos = self._positions[self._step_counts]
+    old_price = self._prices[self._step_counts]
+    self._step_counts += 1
+    new_pos = self._positions[self._step_counts] = max(min(old_pos + ac, MAX_HOLDING), -MAX_HOLDING)
+    new_price = self._prices[self._step_counts] = self._next_price(old_price)
 
     trade_size = abs(new_pos - old_pos)
     cost = TickSize * (trade_size + 1e-2 * trade_size**2)
     PnL = (new_price - old_price) * old_pos - cost
-    self.costs[self.step_counts] = cost
-    self.profits[self.step_counts] = PnL + cost
-    self.rewards[self.step_counts] = PnL - .5 * kappa * PnL**2
+    self._costs[self._step_counts] = cost
+    self._profits[self._step_counts] = PnL + cost
+    self._rewards[self._step_counts] = PnL - .5 * kappa * PnL**2
 
-    done = self.step_counts == self.L
+    done = self._step_counts == self._L
     if done: # liquidate the remaining new_pos shares of the security
       add_cost = TickSize * (abs(new_pos) + 1e-2 * new_pos**2)
       add_PnL = new_price * new_pos - add_cost
-      self.costs[self.step_counts] += add_cost
-      self.profits[self.step_counts] += add_PnL + add_cost
-      self.rewards[self.step_counts] += add_PnL - .5 * kappa * add_PnL**2
+      self._costs[self._step_counts] += add_cost
+      self._profits[self._step_counts] += add_PnL + add_cost
+      self._rewards[self._step_counts] += add_PnL - .5 * kappa * add_PnL**2
+      
+    # Incorporate function property.
+    self._states.append(new_price)
+    if abs(new_pos) > 0:
+      self._actions.append(ac/new_pos)
+    else:
+      self._actions.append(ac)
+    self._rewards[self._step_counts] -= self._learn_func_property(func_property)
 
-    return self.get_state(), self.rewards[self.step_counts], done, {}
+    return self._get_state(), self._rewards[self._step_counts], done, {}
  
   def render(self, mode='human'):
     super(MeanReversionEnv, self).render()
 
-    t = np.linspace(0, self.L, self.L+1)
+    t = np.linspace(0, self._L, self._L+1)
     fig, axs = plt.subplots(3, 1, figsize=(16, 24), constrained_layout=True)
-    axs[0].plot(t, self.prices)
-    axs[1].plot(t, self.positions)
-    axs[2].plot(t, np.cumsum(self.rewards))
+    axs[0].plot(t, self._prices)
+    axs[1].plot(t, self._positions)
+    axs[2].plot(t, np.cumsum(self._rewards))
     axs[0].set_ylabel('price')
     axs[1].set_ylabel('position')
     axs[2].set_ylabel('cumulative P/L')
     plt.title('Out-of-sample simulation of RL agent')
     plt.xlabel('steps')
-    plt.savefig('{}/plot_{}.png'.format(self.folder_name, self.render_counts))
+    plt.savefig('{}/plot_{}.png'.format(self._folder_name, self._render_counts))
     plt.close()
-    plt.plot(t, np.cumsum(self.costs), label='cumulative costs')
-    plt.plot(t, np.cumsum(self.profits), label='cumulative profits')
+    plt.plot(t, np.cumsum(self._costs), label='cumulative costs')
+    plt.plot(t, np.cumsum(self._profits), label='cumulative profits')
     plt.legend()
-    plt.savefig('{}/costs_and_profits_plot_{}.png'.format(self.folder_name, self.render_counts))
+    plt.savefig('{}/costs_and_profits_plot_{}.png'.format(self._folder_name, self._render_counts))
     plt.close()
--- gym-rlf/gym_rlf/envs/rlf_env.py
+++ gym-rlf/gym_rlf/envs/rlf_env.py
@@ -19,35 +19,49 @@ class RLFEnv(gym.Env):
   metadata = {'render.modes': ['human']}
 
   def __init__(self, episode_size, plot_folder_name=None):
-    self.L = episode_size
-    self.render_counts = 0
-    self.folder_name = None
+    self._L = episode_size
+    self._render_counts = 0
+    self._folder_name = None
     if plot_folder_name is not None:
       # Save plots to a newly created folder under plot_folder_name.
-      self.folder_name = plot_folder_name + next(tempfile._get_candidate_names())
+      self._folder_name = plot_folder_name + next(tempfile._get_candidate_names())
 
   def reset(self):
-    self.step_counts = 0
-    self.prices = np.zeros(self.L+1)
-    self.prices[0] = p_e
-    self.positions = np.zeros(self.L+1)
-    self.rewards = np.zeros(self.L+1)
-    self.profits = np.zeros(self.L+1)
-    self.costs = np.zeros(self.L+1)
+    self._step_counts = 0
+    self._prices = np.zeros(self._L+1)
+    self._prices[0] = p_e
+    self._positions = np.zeros(self._L+1)
+    self._rewards = np.zeros(self._L+1)
+    self._profits = np.zeros(self._L+1)
+    self._costs = np.zeros(self._L+1)
+    self._states = []
+    self._actions = []
 
-  def get_state(self):
+  def _get_state(self):
     raise NotImplementedError
+    
+  def get_sharpe_ratio(self):
+    return 16 * np.mean(self._rewards) / np.std(self._rewards)
 
+  def _learn_func_property(self, func):
+    num_past_data = len(self._states) - 1
+    if num_past_data <= 0: return 0
+
+    penalty = 0
+    for i in range(num_past_data):
+      penalty += func(self._states[i], self._states[-1], self._actions[i], self._actions[-1])
+    return penalty / num_past_data
+    
   def step(self, action):
     raise NotImplementedError
 
   def render(self, mode='human'):
     if mode == 'human':
-      self.render_counts += 1
-      if self.folder_name is not None and not os.path.exists(self.folder_name):
-        os.makedirs(self.folder_name)
+      self._render_counts += 1
+      if self._folder_name is not None and not os.path.exists(self._folder_name):
+        os.makedirs(self._folder_name)
 
-      print('sharpe ratio =', 16 * np.mean(self.rewards) / np.std(self.rewards))
+      print('sharpe ratio =', self.get_sharpe_ratio())
     else:
       raise NotImplementedError
 
