c104084428dd09e871c16f8486326cc4a35014d1
Sophia Gu <14866379+sophiagu@users.noreply.github.com>
2020-08-30 00:33:53 -0400

    Add files via upload

--- Scripts/run_baselines_agent.py
+++ Scripts/run_baselines_agent.py
@@ -12,16 +12,16 @@ import optuna
 import os
 
 from functools import partial
-from utils import make_env, ppo2_params, a2c_params
+from utils import make_env, ppo2_params
 from stable_baselines.common.evaluation import evaluate_policy
 from stable_baselines.common.policies import MlpLstmPolicy
 from stable_baselines.common.vec_env import SubprocVecEnv
 from stable_baselines.common.vec_env.vec_normalize import VecNormalize
-from stable_baselines import A2C, PPO2
+from stable_baselines import PPO2
 
 NUM_CPU = multiprocessing.cpu_count()
 
-def _train(env_id, agent, model_params, total_steps, is_evaluation=False):
+def _train(env_id, model_params, total_steps, is_evaluation=False):
   if is_evaluation: # evaluate_policy() must only take one environment
     envs = SubprocVecEnv([make_env(env_id)])
   else:
@@ -29,36 +29,28 @@ def _train(env_id, agent, model_params, total_steps, is_evaluation=False):
   envs = VecNormalize(envs) # normalize the envs during training and evaluation
 
   # Load pretrained model during training.
-  if not is_evaluation and os.path.exists(agent + '_' + env_id):
-    if agent == 'ppo2':
-      model = PPO2.load(agent + '_' + env_id)
-    elif agent == 'a2c':
-      model = A2C.load(agent + '_' + env_id)
+  if not is_evaluation and os.path.exists(env_id):
+    model = PPO2.load(env_id)
   else:
-    if agent == 'ppo2':
-      model = PPO2(MlpLstmPolicy, envs, nminibatches=1, verbose=1, **model_params)
-    elif agent == 'a2c':
-      model = A2C(MlpLstmPolicy, envs, verbose=1, **model_params)
+    model = PPO2(MlpLstmPolicy, envs, n_steps=1, nminibatches=1, verbose=1, **model_params)
 
   model.learn(total_timesteps=total_steps)
   return envs, model
   
-def _search_hparams(env_id, agent, total_steps, trial):
-  if agent == 'ppo2':
-    model_params = ppo2_params(trial)
-  elif agent == 'a2c':
-    model_params = a2c_params(trial)
-
-  envs, model = _train(env_id, agent, model_params, total_steps, True)
-  mean_reward, _ = evaluate_policy(model, envs, n_eval_episodes=10)
+def _search_hparams(env_id, total_steps, trial):
+  envs, model = _train(env_id, ppo2_params(trial), total_steps, True)
+  mean_reward, _ = evaluate_policy(model, envs, n_eval_episodes=2)
   
   envs.close()
   # Negate the reward because Optuna minimizes lost.
   return -mean_reward
 
 def _eval_model(model, env_id, L, ob_shape, num_eps, plot=False):
+  if num_eps <= 2:
+    raise AssertionError('num_eps must be greater than two')
+
   test_env = SubprocVecEnv([make_env(env_id)])
-  sharpe_ratio = 0
+  sharpe_ratios = []
   for episode in range(num_eps):
     # Padding zeros to the test env to match the shape of the training env.
     zero_completed_obs = np.zeros((NUM_CPU,) + ob_shape)
@@ -67,11 +59,13 @@ def _eval_model(model, env_id, L, ob_shape, num_eps, plot=False):
     for _ in range(L):
       action, state = model.predict(zero_completed_obs, state=state, deterministic=True)
       zero_completed_obs[0, :], reward, done, _ = test_env.env_method('step', action[0], indices=0)[0]
-    sharpe_ratio += test_env.env_method('get_sharpe_ratio', indices=0)[0]
+    sharpe_ratios.append(test_env.env_method('get_sharpe_ratio', indices=0)[0])
     if plot: test_env.env_method('render', indices=0)
-
   test_env.close()
-  return sharpe_ratio / num_eps
+  
+  # Compute the average sharpe ratio after removing the highest and the lowest values.
+  sharpe_ratios.sort()
+  return sum(sharpe_ratios[1:-2]) / len(sharpe_ratios[1:-2])
   
 
 if __name__ == '__main__':
@@ -79,73 +73,68 @@ if __name__ == '__main__':
 
   parser = argparse.ArgumentParser()
   parser.add_argument('--env', type=str)
-  parser.add_argument('--agent', type=str, default='ppo2')
   parser.add_argument('--optimize', type=bool, default=False,
-                      help='Whether to search for optimal hyperparameters.')
+                      help='Search for optimal hyperparameters. Drop this flag to run the actual training.')
   parser.add_argument('--num_trials', type=int, default=100,
                       help='Number of trials to search for optimal hyperparameters.')
   parser.add_argument('--num_eps', type=int, default=10,
                       help='Number of episodes to run the final model after training.')
-  parser.add_argument('--evaluation_steps', type=int, default=500000,
+  parser.add_argument('--evaluation_steps', type=int, default=250000,
                       help=('Number of total timesteps that the model runs when evaluating hyperparameters.'
                             'This number must be a multiple of the environment episode size L.'))
-  parser.add_argument('--max_train_steps', type=int, default=5000000,
+  parser.add_argument('--max_train_steps', type=int, default=1000000,
                       help=('Max number of total timesteps that the model runs during training.'
                             'This number must be a multiple of the environment episode size L.'))
   args = parser.parse_args()
-  agent = args.agent.lower()
-  if agent not in ['ppo2', 'a2c']:
-    raise NotImplementedError
-
   if args.env == 'mean_reversion':
     env_id = 'gym_rlf:MeanReversion-v0'
-    study_name = agent + '-mean-reversion-study'
-    L = 1000
+    study_name = 'mean-reversion-study'
   elif args.env == 'apt':
     env_id = 'gym_rlf:APT-v0'
-    study_name = agent + '-apt-study'
-    L = 100
+    study_name = 'apt-study'
   elif args.env == 'delta_hedging':
     env_id = 'gym_rlf:DeltaHedging-v0'
-    study_name = agent + '-delta-hedging-study'
-    L = 100
+    study_name = 'delta-hedging-study'
   else:
     raise NotImplementedError
+
+  L = 100
   study = optuna.create_study(
     study_name=study_name,
-    storage='sqlite:///{}_{}.db'.format(agent, args.env),
+    storage='sqlite:///{}.db'.format(args.env),
     load_if_exists=True)
 
   if args.optimize:
     study.optimize(
-      partial(_search_hparams, env_id, agent, args.evaluation_steps),
+      partial(_search_hparams, env_id, args.evaluation_steps),
       n_trials=args.num_trials,
       n_jobs=NUM_CPU)
-    df = study.trials_dataframe(attrs=('number', 'value', 'params', 'state'))
-    print(df)
+    # df = study.trials_dataframe(attrs=('number', 'value', 'params', 'state'))
+    # print(df)
     print('best hyperparamters found =', study.best_params)
     print('best value achieved =', study.best_value)
     print('best trial =', study.best_trial)
+  else:
+    # Evaluate the model every 10 x L timesteps.
+    # Stop when the evaluation result drops by more than .15.
+    assert args.max_train_steps % (10 * L) == 0
+    best_sr = 0
+    for i in range(args.max_train_steps // (10 * L)):
+      envs, model = _train(env_id, study.best_params, 10 * L)
+      sharpe_ratio = _eval_model(model, env_id, L, envs.observation_space.shape, 12)
+      if sharpe_ratio > best_sr:
+        sharpe_ratio = best_sr
+        model.save(args.env)
+      elif best_sr >= 1 and best_sr - sharpe_ratio >= .15:
+        print('Training stopped after {} episodes with sharpe ratio {}.'.format(i + 1, best_sr))
+        break
+    print('best average sharpe ratio =', best_sr)
+    if not os.path.exists(args.env):
+      print('Training finished without finding a good policy!')
+      model.save(args.env)
 
-  # Evaluate the model every 50 episodes. Stop when the evaluation result drops.
-  assert args.max_train_steps % (50 * L) == 0
-  best_sr = 0
-  for i in range(args.max_train_steps // (50 * L)):
-    envs, model = _train(env_id, agent, study.best_params, 50 * L)
-    sharpe_ratio = _eval_model(model, env_id, L, envs.observation_space.shape, 10)
-    if sharpe_ratio > best_sr:
-      sharpe_ratio = best_sr
-      model.save(agent + '_' + args.env)
-    elif best_sr >= 1:
-      print('Training stopped after {} episodes with sharpe ratio {}.'.format(i + 1, best_sr))
-      break
-  print('best sharpe ratio =', best_sr)
-  model.save(agent + '_' + args.env + '_final')
-
-  del model
-  if agent == 'ppo2':
-    model = PPO2.load(agent + '_' + args.env)
-  elif agent == 'a2c':
-    model = A2C.load(agent + '_' + args.env)
-  _eval_model(model, env_id, L, envs.observation_space.shape, args.num_eps, True)
-  envs.close()
+    del model
+    model = PPO2.load(args.env)
+    sharpe_ratio = _eval_model(model, env_id, L, envs.observation_space.shape, args.num_eps, True)
+    print('average sharpe ratio =', sharpe_ratio)
+    envs.close()
--- Scripts/utils.py
+++ Scripts/utils.py
@@ -16,11 +16,3 @@ def ppo2_params(trial):
     'vf_coef': trial.suggest_uniform('vf_coef', .1, .9),
     'lam': trial.suggest_uniform('lam', .8, 1.)
   }
-
-def a2c_params(trial):
-  return {
-    'learning_rate': trial.suggest_loguniform('learning_rate', 1e-5, 1.),
-    'ent_coef': trial.suggest_loguniform('ent_coef', 1e-8, 1e-1),
-    'vf_coef': trial.suggest_uniform('vf_coef', .1, .9),
-    'lr_schedule': trial.suggest_categorical('lr_schedule', ['constant', 'linear', 'double_linear_con'])
-  }
