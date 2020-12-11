7485bfd7a10f480cf9c08737311cc2c264aab737
Sophia Gu <14866379+sophiagu@users.noreply.github.com>
2020-12-10 21:08:10 -0500

    Update __init__.py

--- gym-rlf/gym_rlf/envs/__init__.py
+++ gym-rlf/gym_rlf/envs/__init__.py
@@ -1,4 +1,5 @@
 from gym_rlf.envs.rlf_env import RLFEnv
 from gym_rlf.envs.mean_reversion_env import MeanReversionEnv
+from gym_rlf.envs.arma_env import ARMAEnv
 from gym_rlf.envs.apt_env import APTEnv
 from gym_rlf.envs.delta_hedging_env import DeltaHedgingEnv
